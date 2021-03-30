8354e5493b07ec34240cbc218dc98bdc3da5794d
Tobin South <tobin.south@gmail.com>
2021-03-30 15:11:45 +1030

    Updating for server usage

--- FB_UoM_data.py
+++ FB_UoM_data.py
@@ -2,25 +2,29 @@ import pickle
 import pandas as pd, numpy as np
 from datetime import *
 
+here = '/home/tobin/COVID-Risk-App'
+here = '/Users/tobin/Dropbox/MPhil/Projects/COVID Risk Mapping/Bandicoot Code'
+
+
 ######################################################################################################################################################################
 # Loading in Facebook Data
 ######################################################################################################################################################################
 
-fake_data = pd.read_csv('data/FB_sample_data.csv')
-fake = False  # Use when debugging avoid calling the server constantly.
+fake_data = pd.read_csv(here+'/data/FB_sample_data.csv')
+fake = True  # Use when debugging avoid calling the server constantly.
 
 import pyodbc, json
-with open('server_credentials.json', 'r') as f:
+with open(here+'/server_credentials.json', 'r') as f:
 	creds = json.load(f)
 
-def get_fb_data(date, time, region):
+def get_fb_data(time, region, start_date, end_date):
 	"""Collects Origin-Destination flow dataframe from UMelb SQL server.
 
     Extended description of function.
 
     Args:
         date (str): Date string in '%Y-%M-%d' (e.g. "2021-01-01")
-        time (int): Choice of time slice between 0000, 0600, 1200. Leading zeros are added automatically.
+        time (str): Choice of time slice between 0000, 0600, 1200 and *. 
 		region (str): Abbreviation of state (e.g. "SA" or "VIC")
 
     Returns:
@@ -28,20 +32,27 @@ def get_fb_data(date, time, region):
 			Columns are LGA19_source, LGA19_target and n_trips.
     """
 
-
 	if fake:  # Use when debugging avoid calling the server constantly.
 		print('Getting fake data')
-		date = datetime.strptime(date, '%Y-%M-%d').strftime('%-d/%-M/%y')
+		date = datetime.strptime(start_date, '%Y-%M-%d').strftime('%-d/%-M/%y')
 		grouped = fake_data.groupby(['date','time', 'region'])
 		ODflows = grouped.get_group((date, time, region))
 		return ODflows
 	else:
 		with pyodbc.connect('DRIVER='+creds["driver"]+';SERVER='+creds["server"]+';DATABASE='+creds["database"]+';UID='+creds["username"]+';PWD='+ creds["password"]) as conn:
-			query = "SELECT * FROM dbo.FB_LGA19_OD WHERE date='%s' AND region = '%s' AND time=%04d" % (date, region, time)
+			query = "SELECT * FROM dbo.FB_LGA19_OD WHERE date>='%s' AND date<='%s' AND region = '%s' AND time=%s" % (
+				start_date, end_date, region, time)
 			response_dataframe = pd.read_sql_query(query, conn)
-			print('Query from database: ', date, time, region, '; Result length = ', len(response_dataframe))
+			# print('Query from database: ', start_date, end_date,  time, region, '; Result length = ', len(response_dataframe))
 			response_dataframe = response_dataframe[~response_dataframe.duplicated()]
-		return response_dataframe
+			response_dataframe.LGA19_source = pd.to_numeric(response_dataframe.LGA19_source)
+			response_dataframe.LGA19_target = pd.to_numeric(response_dataframe.LGA19_target)
+			response_dataframe['time'] = pd.to_numeric(response_dataframe['time'])
+			# response_dataframe['region'] = pd.to_numeric(response_dataframe['region'])
+			response_dataframe = response_dataframe.groupby(
+				['LGA19_source', 'LGA19_target', 'region']).sum().reset_index()
+
+	return response_dataframe
 
 ######################################################################################################################################################################
 # Calculating Risk
@@ -59,7 +70,7 @@ def get_fb_risk(ODflows, locations, state):
         dict: A dictionary of { LGA code : risk estimate } pairs which will be plotted.
 
     """
-	print('Risk locations:', locations)
+	# print('Risk locations:', locations)
 
 	# Keep only flows that are within state
 	ODflows = ODflows[(ODflows.LGA19_source // 10000 == state)  & (ODflows.LGA19_target // 10000 == state)]
@@ -106,10 +117,34 @@ cbd_lat_longs = {
 
 # Useful maps of information
 state_fullname_map = {1: 'New South Wales', 2: 'Victoria', 3: 'Queensland', 4: 'South Australia', 5: 'Western Australia',
-                      6: 'Tasmania', 7: 'Northern Territory', 8: 'Australian Capital Territory', 9: 'Other Territories'}
+                      6: 'Tasmania', 7: 'Northern Territory'}
 state_acronym_map = {4: 'SA', 7: 'NT', 3: 'QLD',
                      5: 'WA', 1: 'NSW', 2: 'VIC', 6: 'TAS'}
 
 # Loading in a dictionary of LGA Codes to Proper Names
-with open("data/lga_name_map.pickle", "rb") as f:
+with open(here+"/data/lga_name_map.pickle", "rb") as f:
 	lga_name_map = pickle.load(f)
+
+
+# An empty figure object to show when there is no data
+empty_graph = {
+    "layout": {
+        "xaxis": {
+            "visible": False
+        },
+        "yaxis": {
+            "visible": False
+        },
+        "annotations": [
+            {
+                "text": "Loading...",
+                "xref": "paper",
+                "yref": "paper",
+                "showarrow": False,
+                "font": {
+                    "size": 20
+                }
+            }
+        ]
+    }
+}
--- app.py
+++ app.py
@@ -19,14 +19,26 @@ from dash_extensions.snippets import send_data_frame
 # Import FB UoM Data Collection
 from FB_UoM_data import *
 
+import logging, sys, os
+# logging.basicConfig(filename=here+'/temp/python.log', level=logging.INFO, 
+#                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
+# # os.chmod(LOG_FILENAME, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO);
+# logging.info('Server should be starting.')
+# logging.info('Python version and path' + sys.version)
+# # for p in sys.path:
+# #     logging.info(p)
+# os.chmod(here+'/temp/python.log', 0o777)
+
+print('Server should be starting')
 ######################################################################################################################################################################
 # This is the setup of the actual dash html interface
 ######################################################################################################################################################################
 
 # import the css template, and pass the css template into dash
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = "Network"
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
+                requests_pathname_prefix='/coviddash/' )  # requests_pathname_prefix='/coviddash/',
+app.title = "COVID-19 Risk Map"
 
 # styles: for right side hover/click component
 styles = {
@@ -38,13 +50,18 @@ styles = {
 
 app.layout = html.Div([
 	# Title & Explainer
-	html.Div([html.H1("COVID Risk Mapping")], className="row",  style={'textAlign': "center"}),
-	# html.Div(className="row", 
-	#     children = dcc.Markdown(d("""
-	#                     About this
-	#                     """)),
-	#         ),
-	html.Div(className="row"),
+	html.Div([html.H1("COVID-19 Risk Mapping")], className="row",  style={'textAlign': "center"}),
+	html.Div(className="row", style={'textAlign': "justify", 'padding-right': '30px', 'padding-left': '30px'},
+		children = dcc.Markdown(d(
+		"""
+		COVID-19 is highly transmissible and containing outbreaks requires a rapid and effective response. Because infection may be spread by people who are pre-symptomatic or asymptomatic, substantial undetected transmission is likely to occur before clinical cases are diagnosed. When outbreaks occur there is a need to anticipate which populations and locations are at heightened risk of exposure.
+
+		This app uses aggregate human mobility data for estimating the geographical distribution of transmission risk. This aggregate data is provided by [Facebook's Data for Good](https://dataforgood.fb.com/) initiative. To learn more about the methods used here, their reliability and their applicability, read [Risk mapping for COVID-19 outbreaks in Australia using mobility data](https://royalsocietypublishing.org/doi/full/10.1098/rsif.2020.0657).
+
+		To use the app select a range of dates and times to aggregate mobility data over for a state. Outbreaks may be started from any set of local government areas. A generalised risk profile will be generated when no locations are selected.
+		"""
+		))),
+	html.Hr(),
 	# Input selectors
 	html.Div(
 		className="row border border-secondary", style={'textAlign': "center"}, 
@@ -54,15 +71,21 @@ app.layout = html.Div([
 				className="three columns", 
 				children=[
 					dcc.Markdown(d("""
-						#### Select date
+						#### Select Dates
 						""")),
-					dcc.DatePickerSingle(
-						id='date_picker',
-						min_date_allowed=date(2020, 5, 1),
-						max_date_allowed=date.today(),
-						date = date(2021, 1, 1),
-					),
-					# html.Div(id='show-date_picker')
+					dcc.DatePickerRange(
+                        id='date_picker',
+                        min_date_allowed=date(2020, 5, 1),
+                        max_date_allowed=date.today(),
+                        start_date = date(2021, 1, 1),
+                        end_date= date(2021, 1, 10)
+                    ),
+					# dcc.DatePickerSingle(
+					# 	id='date_picker',
+					# 	min_date_allowed=date(2020, 5, 1),
+					# 	max_date_allowed=date.today(),
+					# 	date = date(2021, 1, 1),
+					# ),
 				]
 			),
 			# Time picker
@@ -70,20 +93,20 @@ app.layout = html.Div([
 				className="two columns",
 				children=[
 					dcc.Markdown(d("""
-						#### Select times
+						#### Select Times
 						""")),
 					dcc.Dropdown(
 						id = 'time_option',
 						options=[
-							{'label': 'Morning', 'value': 0},
-							{'label': 'Afternoon', 'value': 800},
-							{'label': 'Night', 'value': 1600}
+							{'label': 'Morning', 'value': '0000'},
+							{'label': 'Afternoon', 'value': '0800'},
+							{'label': 'Night', 'value': '1600'},
+							{'label': 'All', 'value': '*'}
 						],
 						value=0,
 						searchable=False
 						# labelStyle={'display': 'inline-block'}
 					),
-					# html.Div(id = "show-selected_times")
 				]
 			),
 			# State picker
@@ -100,10 +123,6 @@ app.layout = html.Div([
 						searchable=False
 						# labelStyle={'display': 'inline-block'}
 					),
-					# dcc.Markdown(d("""
-					# 	Mobility data is only considered within-state. 
-					# 	""")),
-					html.Div(id = "show-selected_times")
 				]
 			),
 			# Location picker
@@ -111,7 +130,7 @@ app.layout = html.Div([
 				className="four columns",
 				children=[
 					dcc.Markdown(d("""
-						#### High Risk Locations
+						#### Outbreak Centres
 						""")),
 					dcc.Dropdown(
 						id='locations',
@@ -119,96 +138,120 @@ app.layout = html.Div([
 						value= [],
 						multi=True
 					),
-					dcc.Checklist(id='select-all-from-dropdown',
-						options=[{'label': 'Select All', 'value': 1}], value=[]),
-					html.Div(id='show-locations'),
+					# dcc.Checklist(id='select-all-from-dropdown',
+					# 	options=[{'label': 'Select All', 'value': 1}], value=[]),
+					# html.Div(id='show-locations'),
 				]
 			)
 		]
 	),
+	html.Div(className="row"),
 	dcc.Markdown(d("""
-		Mobility data from Facebook is collected to quantify the movement of individuals between local government areas (LGA). A risk estimate is generated based on the locations included in High Risk Locations. Risk spreads from these locations into other LGAs where people often travel to after visiting the high risk locations. When no locations are selected, a general risk estimate will be shown.		
-		""")),
+			After selecting the desired inputs, click the Create Risk Estimate button to begin calculation. This may take several seconds to process and display depending on your hardware.
+		"""), style={'textAlign': "center", "margin-top": "15px"}),
 	html.Hr(),
 	# Submit button
-	html.Div(
-		className="row",  style={'textAlign': "center"}, 
-		children=[
-			html.Button("Create Risk Estimate", id="submit_button")
+	dcc.Loading(
+            id="loading-bar",
+            type="default",
+			children=[
+				html.Div(
+					className="row",  style={'textAlign': "center"}, 
+					children=[
+						html.Button("Create Risk Estimate", id="submit_button")
+						]
+				),
+				# Hidden div inside the app that stores the risk values
+				html.Div(id='risk_estimate_variable', style={'display': 'none'}),
+				html.Div(id="loading-output")
 			]
 	),
 	html.Hr(),
 	# Plots 
-	html.Div(
-		className="row",
-		children=[            
-			html.Div(
-				className="eight columns",
-				children=[
-					dcc.Markdown(d("""
-						### Risk Map:
-						""")),
-					dcc.Graph(id="choropleth", 
-						figure= px.choropleth_mapbox([], geojson={}, 
-							locations=[1],  
-							center=cbd_lat_longs[1], 
-							mapbox_style= "carto-positron", zoom=8)
-						)
-				]
-			),
-			html.Div(
-				className="four columns",
-				children=[
-					dcc.Markdown(d("""
-						### Highest Risk Areas:
-						""")),
-					dcc.Graph(id = 'high_risk_areas')
-				]
+	dcc.Loading(
+            id="loading-bar-figures",
+            type="default",
+			children=[
+				html.Div(
+					className="row",
+					children=[            
+						html.Div(
+							className="eight columns",
+							children=[
+								dcc.Markdown(d("""
+									### Risk Map:
+									""")),
+								dcc.Graph(id="choropleth", figure=empty_graph)
+							]
+						),
+						html.Div(
+							className="four columns",
+							children=[
+								dcc.Markdown(d("""
+									### Highest Risk Areas:
+									""")),
+								dcc.Graph(id='high_risk_areas', figure=empty_graph)
+							]
 
-			),
-		]
+						),
+					]
+				),
+			]
 	),
 	html.Hr(),
 	html.Div(
 		className="row",  style={'textAlign': "center"}, 
 		children=[
-			html.Button("Download Flow Matrix as CSV", id="download_button")
+			html.Button("Download Risk Potential as CSV", id="download_button")
 			]
 	),
-
-	# Hidden div inside the app that stores the risk values
-	html.Div(id='risk_estimate_variable', style={'display': 'none'}),
-
 	# Element to allow for file download
 	Download(id="download_div"),
+	html.Hr(),
+	html.Div(
+		className="row",  style={'textAlign': "justify", 'padding-right': '30px', 'padding-left': '30px'},
+		children=dcc.Markdown(d(
+			"""
+			Research associated with this app has demonstrated that mobility data can be a good predictor of geographical patterns of exposure risk from transmission centres, particularly in outbreaks involving workplaces or other environments associated with habitual travel patterns [[1]](https://royalsocietypublishing.org/doi/full/10.1098/rsif.2020.0657). Mobility data add the most value to risk predictions when case counts are low and spatially clustered and can assist health systems in the allocation of testing resources, and potentially guide the implementation of geographically targeted restrictions on movement and social interaction.
+
+			#### Disclaimer 
+
+			The organsiations that authored this app accept no responsibility for the results of any actions taken on the basis of information on this website, nor for the accuracy or completeness of any material contained herein. 
+			
+			Please note this app may discontinue or vary at any time without notice.
+
+			This website contains links to external organisations and information. The organsiations that authored this app accept do not accept responsibility for, nor endorse, the content of any linked site.
+			"""
+			))),
+
 ])
 
 ######################################################################################################################################################################
 # These callbacks are what will make everything interactive
 ######################################################################################################################################################################
 
-# Show selected locations
-@app.callback(Output('show-locations', 'children'), Input('locations', 'value'), State('select-all-from-dropdown', 'value'))
-def display_locations(values, all_option):
-	if len(values)==0:
-		return None #"No locations selected, will show generalized risk estimate."
-	if all_option != []:
-		if all_option[0] == 1:
-			return "All possible locations selected, which is pretty much the same thing as selecting no locations."
-	return None # "Selected %d locations. Risk map will be estimated starting from these locations." % len(values)
-	# return ", ".join(values)
+# # Show selected locations
+# @app.callback(Output('show-locations', 'children'), Input('locations', 'value'), State('select-all-from-dropdown', 'value'))
+# def display_locations(values, all_option):
+# 	if len(values)==0:
+# 		return None #"No locations selected, will show generalized risk estimate."
+# 	if all_option != []:
+# 		if all_option[0] == 1:
+# 			return "All possible locations selected, which is pretty much the same thing as selecting no locations."
+# 	return None # "Selected %d locations. Risk map will be estimated starting from these locations." % len(values)
+# 	# return ", ".join(values)
 
 
-# Select all possible starting locations.
-@app.callback( Output('locations', 'value'),
-	[Input('select-all-from-dropdown', 'value')],
-	[State('locations', 'options'),
-	 State('locations', 'value')])
-def select_all_locations(selected, options, values):
-	if selected != []:
-		if selected[0] == 1:
-			return [i['value'] for i in options]
-	return values
+# # Select all possible starting locations.
+# @app.callback( Output('locations', 'value'),
+# 	[Input('select-all-from-dropdown', 'value')],
+# 	[State('locations', 'options'),
+# 	 State('locations', 'value')])
+# def select_all_locations(selected, options, values):
+# 	if selected != []:
+# 		if selected[0] == 1:
+# 			return [i['value'] for i in options]
+# 	return values
 
 # Update location list according to state
 @app.callback(Output('locations', 'options'),
@@ -219,17 +262,22 @@ def update_possible_state_locations(state):
 
 # Get new data & run risk estimate on submit
 @app.callback(Output('risk_estimate_variable', 'children'), Input('submit_button', 'n_clicks'),
-              state=[State('date_picker', 'date'), State('time_option', 'value'), State('state', 'value'), 
-			  State('locations', 'value')])
-def run_risk_estimate(n_clicks, date, time_option, state, locations): 
-	ODflows = get_fb_data(date, time_option, state_acronym_map[state])
+              state=[State('time_option', 'value'), State('state', 'value'), 
+			  State('locations', 'value'), Input('date_picker', 'start_date'), Input('date_picker', 'end_date')])
+def run_risk_estimate(n_clicks, time_option, state, locations, start_date, end_date): 
+	# if n_clicks:
+	import time
+	time.sleep(2)
+	ODflows = get_fb_data(time_option, state_acronym_map[state], start_date, end_date)
 	risk_estimate = get_fb_risk(ODflows, locations, state) 
 	return json.dumps(risk_estimate)
 
+
+
 # Update Choropleth
 import geopandas
-
-full_geo_df = geopandas.read_file("data/LGA_small_01.geojson")
+print('Reading in geopandas')
+full_geo_df = geopandas.read_file(here+"/data/LGA_small_02.geojson")
 full_geo_df.id = pd.to_numeric(full_geo_df.id)
 full_geo_df.LGA_CODE19 = pd.to_numeric(full_geo_df.LGA_CODE19)
 full_geo_df.AREASQKM19 = pd.to_numeric(full_geo_df.AREASQKM19)
@@ -275,8 +323,8 @@ def update_choropleth(risk_estimate, state, locations):
 		fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
 		return fig
+
 	else:
-		return None
 		print('No risk estimate')
 		fig = px.choropleth_mapbox([], geojson={}, locations=[1],  
                               center=cbd_lat_longs[state], mapbox_style="carto-positron", 
@@ -304,7 +352,8 @@ def update_high_risk_areas(risk_estimate):
 		# md = dcc.Markdown( "\n \n".join(content) ),
 		return fig
 	else:
-		return None
+		print('Showing empty high risk areas graph')
+		return empty_graph
 
 
 # Download CSV on Button Press
@@ -312,7 +361,7 @@ def update_high_risk_areas(risk_estimate):
                state=[State('risk_estimate_variable', 'children'), State('state', 'value')])
 def generate_csv(n_clicks, risk_estimate_variable, state):
 	if risk_estimate_variable:
-		risk_estimate = json.loads(risk_estimate)
+		risk_estimate = json.loads(risk_estimate_variable)
 		risk_estimate = {int(k): v for k, v in risk_estimate.items()}
 
 		state_geo_df = full_geo_df[full_geo_df.STE_CODE16 == state]
@@ -322,10 +371,13 @@ def generate_csv(n_clicks, risk_estimate_variable, state):
 		state_geo_df['Risk Potential'] = [risk_estimate[code]
                                     for code in state_geo_df.index]
 
-		state_geo_df = state_geo_df[["LGA_CODE19" "LGA_NAME19",
+		state_geo_df = state_geo_df[["LGA_CODE19","LGA_NAME19",
                     "STE_NAME16", "AREASQKM19", 'Risk Potential']]
 
 		return send_data_frame(state_geo_df.to_csv, filename="Risk_by_LGA.csv")
+
+
+server = app.server # the Flask app
 
 # Run
 if __name__ == '__main__':
