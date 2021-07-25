5fc26726e597af1fb1d93f6d1bca8ed018e0d3f0
tobinsouth <tobin.south@gmail.com>
2021-07-25 15:04:17 +0930

    Updating Google Data for App

--- data_updater/download_google.sh
+++ data_updater/download_google.sh
@@ -1,5 +1,13 @@
 git pull --rebase
+
+module load arch/haswell
+module load Python/3.6.1-foss-2016b
+source ../virtualenvs/bin/activate
+
 python3 ./data_updater/download_google.py
+
+deactivate
+
 git add ./dashApp/data/google_mobility_australia.csv
 git add ./dashApp/data/google_data.txt
 git commit -am "Updating Google Data for App"
