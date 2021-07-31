60b87fd56e97f6c1e57d223fe2c24ddbc149ecc1
Tobin Max South <a1704567@maths1.rc.adelaide.edu.au>
2021-07-31 16:36:59 +0930

    Updating Google Data for App

--- data_updater/download_google.sh
+++ data_updater/download_google.sh
@@ -1,6 +1,6 @@
 git pull --rebase
 
-/home/a1171654/python3/Python-3.7.0/python ./data_updater/download_google.py
+/home/a1171654/anaconda3/bin/python ./data_updater/download_google.py
 
 git add ./dashApp/data/google_mobility_australia.csv
 git add ./dashApp/data/google_data.txt
